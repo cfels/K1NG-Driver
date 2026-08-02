56ea49a924aa260309856124d4fbc943ef79ac76
cfels <moxiix@proton.me>
2026-08-02 23:13:49 +0200

    do stuff

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo "cleanup!"
 make clean
+rm -rf build
+echo ""
 
 echo "build driver"
 nix-shell --run "make clean && make"
 cp k1ng_driver src/ui/driver_bin
+echo ""
 
 echo "building UI"
-nix-shell --run "mkdir build && cd build && cmake .. && make -j$(nproc)"
+nix-shell --run "mkdir -p build && cd build && cmake .. && make -j$(nproc)"
+ln -sf build/compile_commands.json compile_commands.json
+echo ""
 
 echo "builds done!"
